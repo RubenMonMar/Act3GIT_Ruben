1ef46dccb830e619ef6f28dab69c95445a694ddf
Rubelita <a.ruben.montero.martin@iespablopicasso.es>
2023-11-27 11:12:17 +0100

    Modicifamos el programa NumberSeven

--- src/Tarea3GIT/NumberSeven.java
+++ src/Tarea3GIT/NumberSeven.java
@@ -6,27 +6,38 @@ public class NumberSeven {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		//Titulo del programa
+
+		// Titulo del programa
 		System.out.println("Números de 7 en 7");
-		
-		//Creamos las variables
+
+		// Creamos las variables
 		int numeroInicial = 0;
 		int numeroTerminar = 0;
-		
-		do {
-			//Pedimos valores por teclado 
-		System.out.print("Introduce un número entero:");
-		numeroInicial = Integer.parseInt(sc.nextLine());
-		System.out.print("Introduce otro número entero:");
-		numeroTerminar = Integer.parseInt(sc.nextLine());
-	
-		if (numeroInicial == numeroTerminar) {
-			System.out.println("Datos incoorectos, los número introducidos deben ser distintos");
-		} }while (numeroInicial == numeroTerminar);
-		
-		 for(int i = numeroInicial; i <= numeroTerminar; i += 7) {
-		      System.out.print(i + " ");
-		    }		    
+		int numeroTres = 0;
+
+		do
+
+		{
+			// Pedimos valores por teclado
+			System.out.print("Introduce un número entero:");
+			numeroInicial = Integer.parseInt(sc.nextLine());
+			System.out.print("Introduce otro número entero:");
+			numeroTerminar = Integer.parseInt(sc.nextLine());
+			System.out.print("Introduce el tercer número entero:");
+			numeroTres = Integer.parseInt(sc.nextLine());
+
+			// Si los numero son iguales da un error
+			if (numeroInicial == numeroTerminar) {
+				System.out.println("Datos incoorectos, los número introducidos deben ser distintos");
+			}
+		} while (numeroInicial == numeroTerminar);
+
+		for (int i = numeroInicial; i <= numeroTerminar; i += 7) {
+			System.out.println(i);
+
+		}
+		if ((numeroTres > numeroInicial) && (numeroTres < numeroTerminar)) {
+			System.out.println("Número tres: " + numeroTres);
+		}
 	}
 }
